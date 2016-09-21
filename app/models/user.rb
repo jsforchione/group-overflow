978f33165fd1ea56265ae13cd945cfcece59f5ba
Jessica Forchione <jsforchione@gmail.com>
2016-09-21 16:59:58 -0700

    add authentication to user model:

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,30 @@
 class User < ActiveRecord::Base
   include BCrypt
-  
+
 	has_many :questions
 	has_many :answers
 	has_many :votes
 	has_many :comments
+
+  validates :email, :username, uniqueness: true
+  validates :password_hash, :email, :username, presence: true 
+
+  def password 
+    @password ||= Password.new(password_hash)
+  end 
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password 
+  end 
+
+  def self.authenticate(email, password)
+    @user = User.find_by_email(email)
+    if @user.password == password 
+      return @user 
+    else 
+      return nil 
+    end 
+  end 
+
 end
