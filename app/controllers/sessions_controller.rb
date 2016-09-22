1535e7d959a87a8a784b410a9c541889cd0f9887
Izzy Matos <israeldmatos@gmail.com>
2016-09-22 16:32:00 -0700

    Fixes user authentication and routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,19 @@
-
-post '/sessions' do
-  user = User.authenticate(params[:email], params[:password])
-  redirect "/sessions/new" unless user
-  session["user"] = user.id
-  redirect "/users/#{user.id}"
-end
-
-delete '/sessions' do
-  session.delete(:user) if session[:user]
-  redirect '/questions'
-end
-
 get '/sessions/new' do
   erb :'/sessions/new'
+end
+
+post '/sessions' do
+  @user = User.find_by_email(params[:email])
+  if @user && @user.password == params[:password]
+    session[:user_id] = @user.id
+    redirect "/users/#{@user.id}"
+  else 
+    @errors = ["Username or password did not match."]
+    erb :'sessions/new'
+  end
+end
+
+delete '/sessions/:id' do
+  session[:user_id] = nil if session[:user_id]
+  redirect '/questions'
 end
