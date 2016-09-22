068135406e137ec315e6839d81be979dc8638b75
Jessica Forchione <jsforchione@gmail.com>
2016-09-22 11:36:04 -0700

    update users controller with post route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,3 +12,13 @@ get '/users/:id' do
   redirect '/questions' unless session['user'] == params[:id].to_i
   erb :'/users/show'
 end
+
+post '/users' do 
+  @user = User.new(params[:user])
+  if @user.save
+    redirect "/users/#{@user.id}"
+  else 
+    @errors = ["Invalid"]
+    erb :'users/new'
+  end 
+end 
