e21e450cd2cc47448ceb03bf0eef7f1746322c41
rustyhutto <rhutto92@gmail.com>
2016-09-22 12:17:02 -0700

    Display answers under question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,6 +9,7 @@ end
 
 get '/questions/:id' do 
   @question = Question.find(params[:id])
+  @answers = @question.answers.all
   erb :'questions/show'
 end 
 
@@ -18,7 +19,6 @@ get '/questions/:id/edit' do
 end 
 
 put '/questions/:id' do
-  p "*" * 80
   @question = Question.find(params[:id])
   @question.update(params[:question])
   redirect "/questions/#{@question.id}"
