ab0994e2b1fd494a2fe1dcfb019a6caf4a34ecb3
Ilaria Varriale <ilariasvarriale@gmail.com>
2016-09-22 12:31:26 -0700

    Adds put route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,15 @@ get '/users/new' do
   erb :'users/new'
 end
 
+get '/users/new' do
+  erb :'users/new'
+end
+
+get '/users/:id/edit' do
+  @user = User.find(params[:id])
+  erb :'users/edit'
+end
+
 get '/users/:id' do
   redirect '/sessions/new' unless current_user
   redirect '/questions' unless session['user'] ==params[:id].to_i
@@ -21,4 +30,14 @@ post '/users' do
     @errors = ["Invalid"]
     erb :'users/new'
   end
+end
+
+  put '/users/:id' do
+  @user = User.find(params[:id])
+  if params[:password] == ""
+    @user.update(username: params[:username], email: params[:email], password_hash: @user.password_hash)
+  else
+    @user.update(username: params[:username], email: params[:email], password: params[:password])
+  end
+  redirect "/users/#{@user.id}"
 end
