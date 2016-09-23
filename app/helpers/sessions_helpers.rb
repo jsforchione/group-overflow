711e71a9a9cdf34b0fd604af5fc15225d20dbe85
Izzy Matos <israeldmatos@gmail.com>
2016-09-23 11:33:19 -0700

    Fixes current_user helper method

--- app/helpers/sessions_helpers.rb
+++ app/helpers/sessions_helpers.rb
@@ -1,8 +1,6 @@
 helpers do
   def current_user
-    if session[:user_id]
-		  @user ||= User.find(session[:user_id])
-    end
+		  @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def logged_in?
