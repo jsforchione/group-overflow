6eaeb0d50f9c831073d44e3d7b2dc8f316c0f2ca
Izzy Matos <israeldmatos@gmail.com>
2016-09-22 23:07:04 -0700

    Adds logged_in and log_out sessions helper methods

--- app/helpers/sessions_helpers.rb
+++ app/helpers/sessions_helpers.rb
@@ -1,5 +1,16 @@
 helpers do 
   def current_user
-		@user ||= User.find(session[:user_id]) if session[:user_id]
-  end 
-end 
+    if (user_id = session[:user_id])
+		  @user ||= User.find(user_id)
+    end
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def log_out
+    session[:user_id] = nil 
+    current_user = nil
+  end
+end
