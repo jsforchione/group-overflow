9b001be9774faec90a9d87232bd50cec3ccd6145
rustyhutto <rhutto92@gmail.com>
2016-09-22 19:15:41 -0700

    Add comments to questions and answers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -45,8 +45,19 @@ end
 post '/questions/:id' do
   @question = Question.find(params[:id])
   @answers = @question.answers.create(params[:answer])
+  @question_comment = @question.comments.create(params[:comment])
+  
+
   redirect "/questions/#{params[:id]}"
 end 
+
+post '/questions/:question_id/answers/:id' do
+  @question = Question.find(params[:question_id])
+  @answer = Answer.find(params[:id])
+  @answer_comment = @answer.comments.create(params[:comment])
+
+  redirect "/questions/#{params[:question_id]}"
+end
 
 
 
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,5 +2,5 @@ class Answer < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :question
 	has_many :votes, as: :votable
-	has_many :comment, as: :commentable
+	has_many :comments, as: :commentable
 end
